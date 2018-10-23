d8da136b2f059d6bd1d96eff3120db04df612953
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-10-23 09:57:58 +0800

    Navmesh stuff

---
+++ Passenger/Assets/Scripts/PaCTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaCTarget : MonoBehaviour {
+    PointAndClickMove myPaC;
+    private void Start() {
+        myPaC = GetComponent<PointAndClickMove>();
+    }
+
+    void Update() {
+        Vector3 newTarget = Vector3.zero;
+        RaycastHit hit;
+        if (Input.GetMouseButton(0)) {
+            Debug.Log("I Click mouse you fuck why you no move fucker");
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
+                Debug.Log(hit);
+                newTarget = hit.point;
+                myPaC.UpdateTarget(newTarget);
+            }
+        }
+    }
+}
