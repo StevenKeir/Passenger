f524e70fa6ae17dbe6572307622c4bb9a0c5cf0d
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2018-10-30 15:06:33 +0800

    Steve - inventory start

---
+++ Passenger/Assets/Scripts/InventoryScript/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    void OnMouseOver()
+    {
+        if ( Input.GetMouseButtonDown(0))
+        {
+            print("interacted");
+            Interact();
+        }
+    }
+
+    void Interact()
+    {
+
+    }
+
+}
