c6aca2268db17fab2a4ea5de469c07e124191f88
lodashtaroth <lod_ashtaroth@hotmail.com>
2018-11-06 14:39:31 +0800

    trigger stuff
    ergergh

---
+++ Passenger/Assets/Scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+
+    /* void OnTriggerEnter(Collider collider)
+     {
+         // Check if we ran into a coin
+         if (collider.gameObject.tag == "Player")
+         {
+             print("Dialogue Activate");
+
+
+         } 
+
+     }*/
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Dialogue Activate");
+        //if player triggers object fire dialogue
+        /* if (other.gameObject.CompareTag("Trigger"))
+         {
+             // print("Dialogue Activate");
+             Debug.Log("Dialogue Activate");
+             //Set active dialogue here somehow
+
+            // other.gameObject.name == "Trigger"
+         }*/
+
+    }
+
+   
+}
