e10b9f0614fe861575d52e12f4008e7da09ec952
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-11-07 12:07:47 +0800

    Added stat handler

---
+++ Passenger/Assets/CreateStatCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateStatCollection : MonoBehaviour {
+    public GameObject myStatHandler;
+    private void Start() {
+        if (GameObject.FindGameObjectWithTag("StatHandler") == null) {
+            Instantiate(myStatHandler, transform.position, Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+}
--- Passenger/Assets/StatsHandler.cs
+++ Passenger/Assets/StatsHandler.cs
@@ -4,21 +4,63 @@ using UnityEngine;
 
 public class StatsHandler : MonoBehaviour {
 
-    private static bool created = false;
-
     void Awake() {
-        if (!created) {
-            DontDestroyOnLoad(this.gameObject);
-            created = true;
-            Debug.Log("Awake: " + this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public int busOxygen;
+    public int oxygen;
+    public int calmLevel;
+
+    public bool wearingOxygenKit;
+    public bool outside;
+    public bool hasGun;
+    public bool hasDuctape;
+    public bool hasLaptop;
+    public bool hasCigarBox;
+    public bool hasCrystal;
+
+    public void AffectCalmLevel(int effect) {
+        calmLevel += effect;
+    }
+    public void ChangeLocale(bool inside) {
+        if (!inside) outside = true;
+        else outside = false;
+    }
+    public void AddItem(string myItem, bool add) {
+        switch (myItem) {
+            case "Gun":
+                if (add) hasGun = true;
+                else hasGun = false;
+                break;
+            case "Ductape":
+                if (add) hasDuctape = true;
+                else hasDuctape = false;
+                break;
+            case "Laptop":
+                if (add) hasLaptop = true;
+                else hasLaptop = false;
+                break;
+            case "CigarBox":
+                if (add) hasCigarBox = true;
+                else hasCigarBox = false;
+                break;
+            case "Crystal":
+                if (add) hasCrystal = true;
+                else hasCrystal = false;
+                break;
+            case "OxygenKit":
+                if (add) wearingOxygenKit = true;
+                else wearingOxygenKit = false;
+                break;
         }
     }
-    //all things to do with oxygen, items, and events, should be here.
-    void Start() {
-
+    private void Update() {
+        OxygenDrain();
     }
 
-    void Update() {
-
+    void OxygenDrain() {
+        if (outside) {
+            oxygen += -calmLevel - 1;
+        }
     }
 }
