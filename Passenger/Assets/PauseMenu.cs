38080e4b2288481dfdb5ecad72f62d0335b72e9e
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2018-11-27 11:28:01 +0800

    pause stuff

---
+++ Passenger/Assets/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+
+    public GameObject pauseMenu;
+    public GameObject pauseBackground;
+
+    private void Update()
+    {
+        Pause();
+    }
+
+    void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0.75f;
+            pauseBackground.SetActive(true);
+            pauseMenu.SetActive(true);
+        }
+        
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        pauseBackground.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+
+
+
+}
