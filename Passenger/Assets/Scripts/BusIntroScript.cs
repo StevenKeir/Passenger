aaa27e81ca8f44fa1dea2cd1c2b3efe5782bbf73
lodashtaroth <lod_ashtaroth@hotmail.com>
2018-12-04 11:20:00 +0800

    fucking streaming assets mate

---
+++ Passenger/Assets/Scripts/BusIntroScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusIntroScript : MonoBehaviour {
+
+    public GameObject player;
+    public Animator playerAnim;
+    public GameObject lawyer;
+    public Animator lawyerAnim;
+    public GameObject officer;
+    public Animator officerAnim;
+    public GameObject scientist;
+    public Animator scientistAnim;
+    public GameObject traveller;
+    public Animator travellerAnim;
+
+    public bool seated;
+
+    // Use this for initialization
+    void Start () {
+
+       playerAnim = player.GetComponent <Animator> ();
+       lawyerAnim = lawyer.GetComponent<Animator>();
+       officerAnim = officer.GetComponent<Animator>();
+       scientistAnim = scientist.GetComponent<Animator>();
+       travellerAnim = traveller.GetComponent<Animator>();
+
+        seated = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Seated();
+
+	}
+
+    void Seated() {
+
+        if (seated == true) {
+            playerAnim.SetBool("Seated", true);
+            lawyerAnim.SetBool("Seated", true);
+            officerAnim.SetBool("Seated", true);
+            scientistAnim.SetBool("Seated", true);
+            travellerAnim.SetBool("Seated", true);
+        }
+
+    }
+
+}
