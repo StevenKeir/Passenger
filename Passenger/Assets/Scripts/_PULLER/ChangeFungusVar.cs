056ef63d896708eaea147138f914077e5de33365
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-12-13 12:02:33 +0800

    Update ChangeFungusVar.cs

--- Passenger/Assets/Scripts/_PULLER/ChangeFungusVar.cs
+++ Passenger/Assets/Scripts/_PULLER/ChangeFungusVar.cs
@@ -5,22 +5,40 @@ using Fungus;
 
 public class ChangeFungusVar : MonoBehaviour {
 
+    public string varNameToPull;
+    public int pulledInt;
+    public bool pulledBool;
+    FungusPushPull myFungusPull;
+    [Space(5)]
+    public bool pullInt;
+    public bool pullBool;
+    [Space(15)]
     public string myFlowchartName;
     public string myVarName;
-    [Space(10f)]
+    [Space(15f)]
     [Header("Whether to use Bool or Int. only check one")]
     public bool pushBool;
     public bool pushInt;
-    [Space(10f)]
+    [Space(5f)]
     public int varInt;
     public bool varBool;
 
     List<GameObject> myFlowcharts = new List<GameObject>();
 
     private void Start() {
+        myFungusPull = GameObject.FindGameObjectWithTag("FungusPull").GetComponent<FungusPushPull>();
         GameObject[] myFlowchartObj = GameObject.FindGameObjectsWithTag("Flowchart");
         for (int i = 0; i < myFlowchartObj.Length; i++) {
             myFlowcharts.Add(myFlowchartObj[i]);
+        }
+    }
+
+    private void Update() {
+        if (pullBool) {
+            pulledBool = myFungusPull.PullBool(varNameToPull, pulledBool);
+        }
+        if (pullInt) {
+            pulledInt = myFungusPull.PullInt(varNameToPull, pulledInt);
         }
     }
 
