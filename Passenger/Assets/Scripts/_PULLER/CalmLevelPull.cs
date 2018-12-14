ea01b6f9cc3d3530187d38942d7ef4f012fb5d59
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-12-14 10:35:21 +0800

    update

---
+++ Passenger/Assets/Scripts/_PULLER/CalmLevelPull.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class CalmLevelPull : MonoBehaviour {
+    StatsHandler mySH;
+    //Name of the variable to pull from fungus as a string. case sensitive.
+    string varNameToPull = "CalmLevels";
+    //empty int value to pull. should be 0.
+    public int calmLevel;
+    //reference to the Fungus puller script.
+    FungusPushPull myFungusPull;
+
+    List<GameObject> myFlowcharts = new List<GameObject>();
+
+    private void Start() {
+        mySH = GameObject.FindGameObjectWithTag("StatHandler").GetComponent<StatsHandler>();
+        myFungusPull = GameObject.FindGameObjectWithTag("FungusPull").GetComponent<FungusPushPull>();
+        GameObject[] myFlowchartObj = GameObject.FindGameObjectsWithTag("Flowchart");
+        for (int i = 0; i < myFlowchartObj.Length; i++) {
+            myFlowcharts.Add(myFlowchartObj[i]);
+        }
+    }
+
+    private void Update() {
+            calmLevel = myFungusPull.PullInt(varNameToPull, calmLevel);
+        if (mySH.calmLevel != calmLevel) {
+            mySH.calmLevel = calmLevel;
+        }
+    }
+}
