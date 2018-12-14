5b2ef14d7de3b0aa402302d192bd362da155ff10
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-12-14 11:35:48 +0800

    Push

---
+++ Passenger/Assets/LightWaver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightWaver : MonoBehaviour {
+
+    Light myLight;
+    float timer;
+    public float maxTime;
+    public float minTime;
+    float originalIntensity;
+    float originalRange;
+    bool addTime;
+
+    private void Start() {
+        myLight = GetComponent<Light>();
+        originalIntensity = myLight.intensity;
+        originalRange = myLight.range;
+    }
+
+    private void Update() {
+        TimerChange();
+        myLight.intensity = timer;
+    }
+
+    void TimerChange() {
+        if (addTime) {
+            timer += Time.deltaTime;
+            if (maxTime < timer) {
+                addTime = false;
+            }
+        }
+        else {
+            timer -= Time.deltaTime;
+            if (minTime > timer) {
+                addTime = true;
+            }
+        }
+    }
+}
